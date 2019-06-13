543a7529a09720146f4762724548f7788ef30628
jack <dinglei2008@gmail.com>
2019-06-13 22:18:57 +0800

    Add files via upload

---
+++ game.ex
@@ -0,0 +1,11 @@
+defmodule Game do
+    def start do
+        #1.prepare card
+        #2.create player1, player2
+        #3.prepare cards to player1, player2
+        #4.give the card to player1, player2
+        #5.start the game by send play command to player1,player2 one by one
+        #6.Judge the result and send the cards to the winner or stop the game when one player don't have enough card to play
+        
+    end
+end
